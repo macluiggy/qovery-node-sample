60cf21519c4ff0da3f44a0d39bbd80c86fe2318a
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-10-18 16:23:15 +0200

    fix: host

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const PORT = process.env.HOST || "0.0.0.0"
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, HOST, () => console.log(`Listening on ${ PORT }`))
